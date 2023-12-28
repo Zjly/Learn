0e1d492e913bc39c904d68e5c81a1a70ce89973a
Zhang Lei <942471854@qq.com>
2023-12-28 20:30:20 +0800

    update

---
+++ GNN/链接预测/同构图.py
@@ -0,0 +1,72 @@
+import dgl
+import torch
+import dgl.function as fn
+import dgl.nn as dglnn
+import torch.nn as nn
+import torch.nn.functional as F
+
+dataset = dgl.data.CiteseerGraphDataset()
+graph = dataset[0]
+
+
+class SAGE(nn.Module):
+    def __init__(self, in_feats, hid_feats, out_feats):
+        super().__init__()
+        # 实例化SAGEConve，in_feats是输入特征的维度，out_feats是输出特征的维度，aggregator_type是聚合函数的类型
+        self.conv1 = dglnn.SAGEConv(
+            in_feats=in_feats, out_feats=hid_feats, aggregator_type='mean')
+        self.conv2 = dglnn.SAGEConv(
+            in_feats=hid_feats, out_feats=out_feats, aggregator_type='mean')
+
+    def forward(self, graph, inputs):
+        # 输入是节点的特征
+        h = self.conv1(graph, inputs)
+        h = F.relu(h)
+        h = self.conv2(graph, h)
+        return h
+
+class DotProductPredictor(nn.Module):
+    def forward(self, graph, h):
+        # h是从5.1节的GNN模型中计算出的节点表示
+        with graph.local_scope():
+            graph.ndata['h'] = h
+            graph.apply_edges(fn.u_dot_v('h', 'h', 'score'))
+            return graph.edata['score']
+
+def construct_negative_graph(graph, k):
+    src, dst = graph.edges()
+
+    neg_src = src.repeat_interleave(k)
+    neg_dst = torch.randint(0, graph.num_nodes(), (len(src) * k,))
+    return dgl.graph((neg_src, neg_dst), num_nodes=graph.num_nodes())
+
+class Model(nn.Module):
+    def __init__(self, in_features, hidden_features, out_features):
+        super().__init__()
+        self.sage = SAGE(in_features, hidden_features, out_features)
+        self.pred = DotProductPredictor()
+    def forward(self, g, neg_g, x):
+        h = self.sage(g, x)
+        return self.pred(g, h), self.pred(neg_g, h)
+
+def compute_loss(pos_score, neg_score):
+    # 间隔损失
+    n_edges = pos_score.shape[0]
+    return (1 - pos_score.unsqueeze(1) + neg_score.view(n_edges, -1)).clamp(min=0).mean()
+
+node_features = graph.ndata['feat']
+n_features = node_features.shape[1]
+k = 5
+model = Model(n_features, 100, 100)
+opt = torch.optim.Adam(model.parameters())
+for epoch in range(10):
+    negative_graph = construct_negative_graph(graph, k)
+    pos_score, neg_score = model(graph, negative_graph, node_features)
+    loss = compute_loss(pos_score, neg_score)
+    opt.zero_grad()
+    loss.backward()
+    opt.step()
+    print(loss.item())
+
+node_embeddings = model.sage(graph, node_features)
+print(node_embeddings)
