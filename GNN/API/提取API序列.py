8a872422c5977b047c1a5fb700474c7a4d408bab
Zhang Lei <942471854@qq.com>
2023-12-29 13:10:41 +0800

    update

---
+++ GNN/API/提取API序列.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df = pd.read_feather("train.feather")
+
+df["label"] = df["api_sequence"].apply(lambda x: x[-1])
+df["api_sequence"] = df["api_sequence"].apply(lambda x: x[:-1])
+df.drop("question", axis=1, inplace=True)
+
+df.to_feather("dataset.feather")
--- GNN/整图分类/同构图.py
+++ GNN/整图分类/同构图.py
@@ -1,21 +1,5 @@
 import dgl
 import torch
-
-g1 = dgl.graph(([0, 1], [1, 0]))
-g1.ndata['h'] = torch.tensor([1., 2.])
-g2 = dgl.graph(([0, 1], [1, 2]))
-g2.ndata['h'] = torch.tensor([1., 2., 3.])
-
-dgl.readout_nodes(g1, 'h')
-# tensor([3.])  # 1 + 2
-
-bg = dgl.batch([g1, g2])
-dgl.readout_nodes(bg, 'h')
-# tensor([3., 6.])  # [1 + 2, 1 + 2 + 3]
-
-bg.ndata['h']
-# tensor([1., 2., 1., 2., 3.])
-
 import dgl.nn.pytorch as dglnn
 import torch.nn as nn
 
